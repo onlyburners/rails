eff9ea934cf58b68d7f942df822e939f7f8e2aca
dvidsilva <contacto@dvidsilva.com>
2022-10-21 12:49:48 -0400

    fixed nav view, aded has_settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_settings do |s|
+    s.key :notifications, :defaults => { :email => 'true' }
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_221159) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_160127) do
+  create_table "settings", force: :cascade do |t|
+    t.string "var", null: false
+    t.text "value"
+    t.string "target_type", null: false
+    t.integer "target_id", null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
+    t.index ["target_type", "target_id", "var"], name: "index_settings_on_target_type_and_target_id_and_var", unique: true
+    t.index ["target_type", "target_id"], name: "index_settings_on_target_type_and_target_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
